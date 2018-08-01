ef91f0ed90c470d55629dc3951c48d286777af1d
Jon Sung <jonsung89@gmail.com>
2018-08-01 15:41:27 -0500

    created new POST PUT and DELETE endpoints

--- router/notes.router.js
+++ router/notes.router.js
@@ -2,25 +2,91 @@
 
 const express = require('express');
 const router = express.Router();
-const data = require('../db/notes');
 
 // Load array of notes
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
 
+router.get('/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
 
-router.get('/api/notes', (req, res) => {
-  const searchTerm = req.query.searchTerm;
-  let filteredData = data;
-  if (searchTerm) {
-    filteredData = filteredData.filter(item => JSON.stringify(item).includes(searchTerm));
-  }
-
-  res.json(filteredData);
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
 });
 
-router.get('/api/notes/:id', (req, res) => {
+router.get('/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  notes.find(id, (err, item, next) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(item); // responds with filtered array
+  });
+});
+
+router.put('/notes/:id', (req, res, next) => {
   const id = req.params.id;
-  const filteredData = data.find(item => item.id === Number(id));
-  res.json(filteredData);
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust usters - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.delete('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  notes.delete(id, err => {
+    if (err) {
+      return next(err);
+    }
+    res.sendStatus(204);
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,18 +2,44 @@
 
 const express = require('express');
 const morgan = require('morgan');
-const itemsRouter = require('./router/notes.router');
+
+const {PORT} = require('./config');
+const notesRouter = require('./router/notes.router');
+
+// Create an Express application
 const app = express();
 
+// Log all requests
 app.use(morgan('dev'));
 
-// INSERT EXPRESS APP CODE HERE...
+// Create a static webserver
 app.use(express.static('public'));
 
-// mount the router with app.use(...)
-app.use(itemsRouter);
+// Parse request body
+app.use(express.json());
 
-app.listen(8080, function () {
+// Mount router on "/api"
+app.use('/api', notesRouter);
+
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+// NOTE: we'll prevent stacktrace leak in later exercise
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+// Listen for incoming connections
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
